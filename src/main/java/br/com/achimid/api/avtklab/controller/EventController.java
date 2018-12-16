2ba77ae629052ace0032d1523e7ef59581530b96
Achimid <achimid@hotmail.com>
2018-12-16 16:26:11 -0200

    subindo deploy

--- src/main/java/br/com/achimid/api/avtklab/controller/EventController.java
+++ src/main/java/br/com/achimid/api/avtklab/controller/EventController.java
@@ -6,9 +6,14 @@ import br.com.achimid.api.avtklab.model.Event;
 import br.com.achimid.api.avtklab.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.io.IOException;
 
 @CrossOrigin
 @RestController
@@ -34,12 +39,13 @@ public class EventController implements EventControllerDoc {
     }
 
     @PostMapping
-    public HttpEntity<Event> create(@Validated @RequestBody EventDTO event) {
+    public HttpEntity<Event> create(@Valid @RequestBody EventDTO event) {
         return ResponseEntity.ok(eventService.save(event.getEvent()));
     }
 
     @PutMapping
     public HttpEntity<?> update(@Validated @RequestBody EventDTO event) {
+        if(event.getId() == null) return ResponseEntity.badRequest().build();
         return ResponseEntity.ok(eventService.save(event.getEvent()));
     }
 
@@ -48,5 +54,10 @@ public class EventController implements EventControllerDoc {
         if(!eventService.exists(id)) return ResponseEntity.notFound().build();
         eventService.deleteById(id);
         return ResponseEntity.ok().build();
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public void handleBadRequests(HttpServletResponse response) throws IOException {
+        response.sendError(HttpStatus.BAD_REQUEST.value());
     }
 }
